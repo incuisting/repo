d4a2a8208fd229e2ddbc477aeb2b51cec37bcaf8
incuisting <xuyanting9634@163.com>
2017-08-01 21:11:13 +0800

    制作了一个Welcome组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Welcome from './Welcome'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render(
-    <h1> hi, world! </h1>,
-    document.getElementById('root2')
-);
+    <Welcome/>,
+    document.getElementById('root')
+)
