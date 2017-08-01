478166b264a0133b7b28844b93af6698c8f99ffe
incuisting <xuyanting9634@163.com>
2017-08-01 22:19:02 +0800

    演示挂载（mount）的hook

--- src/Welcome.js
+++ src/Welcome.js
@@ -11,15 +11,22 @@ class Welcome extends React.Component {
                 date: new Date() //update date
             })
         })
+        console.log('我已经在constructor 里将props和state初始化好了')
     }
-
+    componentWillMount(){
+        console.log('运行到这里的话，说明马上就要运行render了')
+    }
     render() {
+        console.log('这里是render')
         return (
             <div>
                 <h1>Hello,{this.props.name}</h1>
                 <h2>{this.state.date.toString()}</h2>
             </div>
         )
+    }
+    componentDidMount(){
+        console.log('已经挂载到页面里了')
     }
 }
 
