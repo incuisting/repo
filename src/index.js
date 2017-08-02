7ae3c3614770cd08940ceec7292be0e56f52d259
incuisting <xuyanting9634@163.com>
2017-08-02 20:08:24 +0800

    添加normalize.css 和 reset.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Welcome from './Welcome'
-
+import App from './App'
 import registerServiceWorker from './registerServiceWorker';
 
 
 
 ReactDOM.render(
-    <Welcome name="react"/>,
+    <App/>,
     document.getElementById('root')
 )
