40c185ea350186d89cc3011dd4f9b437163a2e7d
incuisting <xuyanting9634@163.com>
2017-08-02 22:48:05 +0800

    更新 TodoInput 的样式

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import './TodoInput.css'
 
 export default class TodoInput extends Component {
     render() {
         return (
-            <input type="text" defaultValue={this.props.content}
+            <input type="text" className="TodoInput" defaultValue={this.props.content}
                    onChange={this.changeTitle.bind(this)}
                    onKeyPress={this.submit.bind(this)}/>
         )
