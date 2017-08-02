e4b18ec2fcfcae5cabf02f3a7339581ac6dbe3bc
incuisting <xuyanting9634@163.com>
2017-08-02 20:36:25 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,7 +1,16 @@
-import React , {Component} from 'react';
+import React, {Component} from 'react';
 
-export default class TodoInput extends Component{
-    render(){
-        return <input type="text" defaultValue={this.props.content}/>
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <input type="text" defaultValue={this.props.content}
+                   onkeypress={this.submit}/>
+        )
+    }
+
+    submit(event) {
+        if (event.key === 'Enter') {
+            console.log('用户按了回车');
+        }
     }
 }
