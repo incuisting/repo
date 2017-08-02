d736989503db2e0b37e3dad21c84cef89bcdefd3
incuisting <xuyanting9634@163.com>
2017-08-02 22:33:31 +0800

    删除todo

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ class App extends Component {
         super(props)
         this.state = {
             newTodo: '',
-            todoList: [
-
-            ]
+            todoList: []
         }
     }
 
@@ -20,7 +18,9 @@ class App extends Component {
         let todos = this.state.todoList.map((item, index) => {
             return (
                 <li key={item.id}>
-                    <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
+                    <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                              onDelete={this.delete.bind(this)}
+                    />
                 </li>
             )
         })
@@ -31,7 +31,7 @@ class App extends Component {
                 <h1>我的待办</h1>
                 <div className="inputWrapper">
                     <TodoInput content={this.state.newTodo}
-                               onChange = {this.changeTitle.bind(this)}
+                               onChange={this.changeTitle.bind(this)}
                                onSubmit={this.addTodo.bind(this)}/>
                 </div>
                 <ol>
@@ -41,26 +41,33 @@ class App extends Component {
 
         )
     }
-    toggle(event,todo){
-        todo.status = todo.status === 'completed' ? '':'completed'
+    delete(event,todo){
+        todo.deleted = true
+        this.setState(this.state)
+        console.log('delete',todo.deleted)
+    }
+    toggle(event, todo) {
+        todo.status = todo.status === 'completed' ? '' : 'completed'
         this.setState(this.state)
     }
-    changeTitle(event){
+
+    changeTitle(event) {
         this.setState({
-            newTodo:event.target.value,
-            todoList:this.state.todoList
+            newTodo: event.target.value,
+            todoList: this.state.todoList
         })
     }
-    addTodo(event){
+
+    addTodo(event) {
         this.state.todoList.push({
-            id:idMaker(),
-            title:event.target.value,
-            status:null,
-            deleted:false
+            id: idMaker(),
+            title: event.target.value,
+            status: null,
+            deleted: false
         })
         this.setState({
-            newTodo:'',
-            todoList:this.state.todoList
+            newTodo: '',
+            todoList: this.state.todoList
         })
     }
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,8 +7,13 @@ export default class TodoItem extends Component {
                 <input type="checkbox" checked={this.props.todo.status === 'completed'}
                 onChange={this.toggle.bind(this)}/>
                 {this.props.todo.title}
+                <button onClick={this.delete.bind(this)}>删除</button>
             </div>
         )
+    }
+    delete(event){
+        console.log(1)
+        this.props.onDelete(event,this.props.todo)
     }
     toggle(event){
         this.props.onToggle(event,this.props.todo)
