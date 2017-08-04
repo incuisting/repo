237e0b1d91a8702e2bbf669dcfd25d282996c3f1
incuisting <xuyanting9634@163.com>
2017-08-04 21:03:40 +0800

    添加登录/注册对话框-UserDialog

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render( <
-    h1 > Hello, world! < /h1>,
+ReactDOM.render(
+    <App/>,
     document.getElementById('root')
 );
