0bde71a624dc891186b0a57099a773ffb44fa3f5
incuisting <xuyanting9634@163.com>
2017-08-04 20:28:46 +0800

    新建leanCloud.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import * as localStore from './localStorage'
 
-import AV from 'leancloud-storage'
-let APP_ID='f0B88b8emKpHwXnk69GJw9O9-gzGzoHsz'
-let  APP_KEY='cGr1lluaXyF5n07mLlfQVRgY'
-AV.init({
-    appId:APP_ID,
-    appKey:APP_KEY
-})
+
 
 
 class App extends Component {
---
+++ src/leanCloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage'
+
+let APP_ID = 'f0B88b8emKpHwXnk69GJw9O9-gzGzoHsz'
+let APP_KEY = 'cGr1lluaXyF5n07mLlfQVRgY'
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+})
+export default AV
