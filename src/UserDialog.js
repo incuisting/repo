a454b368babf09c1c43f06945662d62609fc9fd7
incuisting <xuyanting9634@163.com>
2017-08-04 22:39:13 +0800

    在注册和登录表单间切换

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -15,7 +15,42 @@ export default class UserDialog extends Component {
         })
     }
 
+
+
     render() {
+        let
+            signUpForm = (
+                <form className="signUp">
+                    <div className="row">
+                        <label>用户名</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="row">
+                        <label>密码</label>
+                        <input type="password"/>
+                    </div>
+                    <div className="row actions">
+                        <button type="submit">注册</button>
+                    </div>
+                </form>
+            )
+
+        let
+            signInForm = (
+                <form className="signIn">
+                    <div className="row">
+                        <label>用户名</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="row">
+                        <label>密码</label>
+                        <input type="password"/>
+                    </div>
+                    <div className="row actions">
+                        <button type="submit">登入</button>
+                    </div>
+                </form>
+            )
         return (
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
@@ -29,32 +64,8 @@ export default class UserDialog extends Component {
 
                     </nav>
                     <div className="panes">
-                        <form className="signUp">
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text"/>
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password"/>
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">注册</button>
-                            </div>
-                        </form>
-                        <form className="signIn">
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text"/>
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password"/>
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">登入</button>
-                            </div>
-                        </form>
+                        {this.state.selected === 'signUp' ? signUpForm : null}
+                        {this.state.selected === 'signIn' ? signInForm : null}
                     </div>
                 </div>
             </div>
