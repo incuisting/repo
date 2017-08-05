fb5403fc08bd361f139b30bc629a1e13b540bd05
incuisting <xuyanting9634@163.com>
2017-08-05 10:45:59 +0800

    展示友好的错误信息

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -27,7 +27,14 @@ export default class UserDialog extends Component {
             this.props.onSignUp.call(null, user)
         }
         let error = (error) => {
-            console.log(error)
+            switch (error.code){
+                case 202:
+                    alert('用户名已被占用')
+                    break
+                default:
+                    alert(error)
+                    break
+            }
         }
         signUp(username, password, success, error)
     }
@@ -39,7 +46,14 @@ export default class UserDialog extends Component {
             this.props.onSignUp.call(null, user)
         }
         let error = (error) => {
-            console.log(error)
+            switch (error.code){
+                case 210:
+                    alert('用户名与密码不匹配')
+                    break
+                default:
+                    alert(error)
+                    break
+            }
         }
         signIn(username, password, success, error)
 
