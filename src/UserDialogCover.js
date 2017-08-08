8eccf48f255318b0be8bfff7b4d63abb86c1e864
incuisting <xuyanting9634@163.com>
2017-08-08 22:56:31 +0800

    完成左边的登陆cover，tip：svg引入有一个坑

--- src/UserDialogCover.js
+++ src/UserDialogCover.js
@@ -1,9 +1,13 @@
 import  React from 'react'
+import './UserDialogCover.css'
 
 export default function UserDialogCover (props) {
     return(
         <div className="cover">
-
+            <svg className="icon">
+                <use xlinkHref="#icon-icon-test1"></use>
+            </svg>
+            <h2>Duo Duan Shi Yong</h2>
         </div>
     )
 }
